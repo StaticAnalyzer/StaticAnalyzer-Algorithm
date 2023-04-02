60c5c486208309bc03332dd1cc2b4ec668b25fd5
wangli <iswangli@foxmail.com>
2023-04-02 18:31:49 +0800

    feat: 添加算法服务网络接口

--- tools/gRPCServer/server.cpp
+++ tools/gRPCServer/server.cpp
@@ -12,6 +12,8 @@
 
 #include "algservice.grpc.pb.h"
 
+#include "analysis/AnalysisFactory.h"
+
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
@@ -19,6 +21,16 @@ using grpc::Status;
 using algservice::JustReturnRequest;
 using algservice::JustReturnResponse;
 using algservice::AlgService;
+
+#if __has_include(<filesystem>)
+  #include <filesystem>
+  namespace fs = std::filesystem;
+#elif __has_include(<experimental/filesystem>)
+  #include <experimental/filesystem> 
+  namespace fs = std::experimental::filesystem;
+#else
+  error "Missing the <filesystem> header."
+#endif
 
 class AlgServiceUtils {
 public:
@@ -38,22 +50,64 @@ class AlgServiceImpl final : public AlgService::Service {
   Status JustReturn(ServerContext* context, const JustReturnRequest* request,
                     JustReturnResponse* reply) override {
     std::string file = request->file();
+    std::string config = request->config();
 
     // 保存文件到本地
-    std::string filename = AlgServiceUtils::RandomStr(20);
-    std::ofstream out(filename + ".tar.gz", std::ios::binary);
-    out.write(file.c_str(), file.length());
-    out.close();
+    std::string dir = AlgServiceUtils::RandomStr(20);
+    fs::path project_root(dir);
+    fs::path source_dir = project_root / "source";
+    fs::create_directories(source_dir);  // 创建目录
+
+    std::string code_filename = dir + ".tar.gz";
+    std::ofstream code_out(project_root / code_filename, std::ios::binary);
+    code_out.write(file.c_str(), file.length());
+    code_out.close();
 
     // 解压缩文件
-    std::string cmd = "mkdir " + filename + ";" + "tar -zxvf " + filename + ".tar.gz -C " + filename;
+    std::string cmd = "tar -zxvf " + (project_root / code_filename).string() + " -C " + source_dir.string();
     system(cmd.c_str());
 
+    // 保存配置文件
+    std::string config_filename = "config.txt";
+    std::ofstream config_out(project_root / config_filename, std::ios::binary);
+    config_out.write(config.c_str(), config.length());
+    config_out.close();
+
+    // 生成ast文件和astList.txt，这里先假定只有一个文件
+    fs::directory_iterator dir_ite(source_dir);
+    std::list<std::string> ast_list;
+    for(const auto& ite : dir_ite)
+    {
+      if(ite.status().type() == fs::file_type::regular)
+      {
+        std::string filename = ite.path().filename().string();
+        // filenmae以.cpp结尾
+        if(filename.rfind(".cpp") != filename.length() - 4)
+          continue;
+
+        std::string ast_path = ite.path().parent_path().string() + "/" + filename + ".ast";
+        std::string cmd = "clang++ -emit-ast -c " + ite.path().string() + " -o " + ast_path;
+        system(cmd.c_str());
+        ast_list.push_back(ast_path);
+      }
+    }
+    std::ofstream astlist_out(project_root / "astlist.txt", std::ios::binary);
+    for(const auto& ite : ast_list)
+    {
+      astlist_out << ite << std::endl;
+    }
+    astlist_out.close();
+
+
     // 执行算法
-    (*reply->mutable_result())["test"] = "test";
+    analysis::AnalysisFactory analysisFactory(
+        project_root / "astlist.txt",
+        project_root / "config.txt");
+    std::unique_ptr<analysis::Analysis> echo = analysisFactory.createEchoAnalysis();
+    *reply->mutable_result() = echo->analyze();
 
     // 清除临时文件
-    cmd = "rm -rf " + filename + ".tar.gz " + filename;
+    cmd = "rm -rf " + project_root.string();
     system(cmd.c_str());
 
     return Status::OK;
