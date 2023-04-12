630a50dd4c8c2880c4691c26e9fa8cd771b5c1ff
JacyCui <jiacaicui@163.com>
2023-04-12 23:57:12 +0800

    fix: delete unused fields

--- tools/gRPCServer/server.cpp
+++ tools/gRPCServer/server.cpp
@@ -118,9 +118,9 @@ class AlgServiceImpl final : public AlgService::Service {
       analysis::AnalysisFactory analysisFactory(
           project_root / "astlist.txt",
           project_root / "config.txt");
-      std::unique_ptr<analysis::Analysis> echo = analysisFactory.createUninitializedVariableAnalysis();
-      echo->analyze();
-      *reply->mutable_result() = echo->getResult().dump();
+      std::unique_ptr<analysis::Analysis> uni = analysisFactory.createUninitializedVariableAnalysis();
+      uni->analyze();
+      *reply->mutable_result() = uni->getResult().dump();
     }
     catch(const std::exception& e)
     {
